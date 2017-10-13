6b62bba3c55479f00c2d3e5dd57c5821d7f1725f
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-13 23:42:51 +0200

    Created state domain

---
+++ src/main/java/ConstructABetterSociety/domain/State.java
@@ -0,0 +1,37 @@
+package ConstructABetterSociety.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class State extends UriEntity<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Country country;
+
+    private String name;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
