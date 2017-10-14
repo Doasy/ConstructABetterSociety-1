0584ca934d8f88c301e992849f85bd7f5152e683
Alba Lamas <alba.v.lamas@gmail.com>
2017-10-14 17:50:49 +0200

    more dynamic

--- client/js/newComment.js
+++ client/js/newComment.js
@@ -2,6 +2,6 @@ document.getElementById("new-comment-button").onclick = function() {myFunction()
 
 function myFunction() {
     document.getElementById("text").innerHTML =
-        '<textarea rows="4"></textarea>' +
-        '<a id="new-comment-button" href="#" class="btn btn-dark btn-lg js-scroll-trigger">Send</a>'
+        '<textarea id="comment-text" rows="4"></textarea>' +
+        '<a id="send-comment" href="#" class="btn btn-dark btn-lg js-scroll-trigger">Send</a>'
 }
