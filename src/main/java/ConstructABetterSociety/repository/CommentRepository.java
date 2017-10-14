ba49fa41812ecbcfe7bb6538492caa8b5c421782
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-14 18:24:24 +0200

    Added user serializer

--- src/main/java/ConstructABetterSociety/repository/CommentRepository.java
+++ src/main/java/ConstructABetterSociety/repository/CommentRepository.java
@@ -2,10 +2,12 @@ package ConstructABetterSociety.repository;
 
 import ConstructABetterSociety.domain.Comment;
 
+import ConstructABetterSociety.domain.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.Projection;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
@@ -15,4 +17,12 @@ import java.util.List;
 public interface CommentRepository extends PagingAndSortingRepository<Comment, Long> {
     @Query("SELECT c FROM Comment c WHERE c.place.name = :place")
     List<Comment> findByPlace(@Param("place") String place);
+}
+
+@Projection(name = "inlineAddress", types = { User.class })
+interface UserInlineCommentsProjection{
+    String getId();
+    String getEmail();
+    String getName();
+    String getSurname();
 }
