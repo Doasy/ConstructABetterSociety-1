7766c5ee467b645cf71f67e7c574124633f34583
Marc Ribalta Gene <mariva1995@gmail.com>
2017-10-15 03:30:28 +0200

    created profile

--- client/js/decideProfile.js
+++ client/js/decideProfile.js
@@ -1,4 +1,15 @@
 
 $.ajax({
-
+    url: "http://localhost:8080/users/search/findByNickname",
+    dataType: "json",
+    data: {
+        nickname: "Isomorfisme"
+    }
+}).then(function (user) {
+    $('#nickname').append(user.id);
+    $('#name').append(user.name+" "+user.surname);
+    $('#email').append(user.email);
+    $('#rate').append(user.rate);
+    $('#votedTimes').append(user.numberOfTimesRated);
+    $('#coins').append(user.coins);
 });
